8690d1ab7d9bb5a5108059a50148fcfd2f6b157d
HeftyFair <124026807+HeftyFair@users.noreply.github.com>
2025-01-23 12:49:51 +0800

    update

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -18,7 +18,9 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      -- colorscheme = "catppuccin-mocha",
+      colorscheme = "cyberdream",
+
     },
   },
 
@@ -66,17 +68,17 @@ return {
   -- },
   --
   -- -- add pyright to lspconfig
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   ---@class PluginLspOpts
-  --   opts = {
-  --     ---@type lspconfig.options
-  --     servers = {
-  --       -- pyright will be automatically installed with mason and loaded with lspconfig
-  --       pyright = {},
-  --     },
-  --   },
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        denols = {},
+      },
+    },
+  },
   --
   -- -- add tsserver and setup with typescript.nvim instead of lspconfig
   -- {
