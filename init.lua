a2c81e74f87ed14da832960be0a3d81198599576
HeftyFair <kwqcheii@outlook.com>
2025-02-12 22:42:30 +0800

    myfiles

--- init.lua
+++ init.lua
@@ -2,9 +2,13 @@
 require("config.lazy")
 
 -- vim.g.neovide_show_border = true
-vim.g.neovide_window_blurred = true
-vim.g.neovide_transparency = 0.75
-vim.g.neovide_scroll_animation_far_lines = 1
-vim.g.neovide_unlink_border_highlights = false
-vim.g.neovide_hide_mouse_when_typing = true
-vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+--
+-- if vim.g.neovide then
+  vim.g.neovide_window_blurred = true
+  vim.g.neovide_transparency = 0.75
+  vim.g.neovide_scroll_animation_far_lines = 1
+  vim.g.neovide_unlink_border_highlights = false
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_input_macos_option_key_is_meta = "only_left"
+  vim.g.neovide_cursor_vfx_mode = "ripple"
+-- end
